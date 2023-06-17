b0e5e24d1f6aa45bee518f238c9eeb93e4a981c4
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2023-06-18 00:58:17 +0600

    Live reload of php add

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import create_config from '@kucrut/vite-for-wp';
 import react from '@vitejs/plugin-react';
+import liveReload from 'vite-plugin-live-reload';
 
 // https://vitejs.dev/config/
 export default {
   //configuration
-  ...create_config('', 'dist', { plugins: [react()] }),
+  ...create_config('', 'dist', { 
+    plugins: [
+      react(),
+      liveReload(`${__dirname}/**/*\.php`),
+    ],
+  }),
 };
