c538300303f13836c2e9ae6979c4a720086b4ef9
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2023-06-20 11:07:48 +0600

    Shortcode add to plugin admin page

--- src/admin/Components/AdminNotice/AdminNotice.tsx
+++ src/admin/Components/AdminNotice/AdminNotice.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./AdminNotice.scss";
 
 export default function AdminNotice() {
+  const [copied, setCopied] = React.useState(false);
   return (
     <>
       <a
@@ -12,9 +13,33 @@ export default function AdminNotice() {
           LIILab Plugin Boilerplate
         </h5>
         <p className="font-normal text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
+         Copy this shortcode and paste it into your page or post.
         </p>
+        <div className="flex items-center justify-between mt-4">
+        <span
+            style={{
+              cursor: "pointer",
+              backgroundColor: "#dcdcde",
+              padding: "8px 10px",
+              borderRadius: "2px",
+              transition: "opacity 0.3s",
+              opacity: copied ? 0.5 : 1,
+            }}
+            onClick={(e: any) => {
+              e.preventDefault();
+              const valueToCopy = String("[liilab_current_user_info]");
+              navigator.clipboard.writeText(valueToCopy);
+              setCopied(true);
+
+              // Reset the copied state after a delay
+              setTimeout(() => {
+                setCopied(false);
+              }, 5000);
+            }}
+          >
+            {"[liilab_current_user_info]"}
+          </span>
+          </div>
       </a>
     </>
   );
